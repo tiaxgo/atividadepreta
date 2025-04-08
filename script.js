5993f3e70d2d8b99c1cc8f66fa7da7e5fa3ad283
tiaxgo <161162502+tiaxgo@users.noreply.github.com>
2025-04-08 03:29:47 -0300

    up

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 // Import Swiper and styles
-    const swiper = new Swiper('.swiper', {
-      loop: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-        pauseOnMouseEnter: true,
-      },
-      slidesPerView: 3,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      },
-      breakpoints: {
-        768: {
-          slidesPerView: 4
-        },
-        1024: {
-          slidesPerView: 5
-        }
-      }
-    });
+    <script>
+        const swiper = new Swiper('.swiper', {
+          loop: true,
+          autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          },
+          slidesPerView: 3,
+          spaceBetween: 30,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          },
+          breakpoints: {
+            768: {
+              slidesPerView: 4
+            },
+            1024: {
+              slidesPerView: 5
+            }
+          }
+        });
+    </script>
