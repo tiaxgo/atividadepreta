9a483e751ed8232bbff889889ae383b28fd0b71a
Tiago Martins de Almeida <tiiagoalmeiida2001@gmail.com>
2025-04-16 04:00:07 -0300

    up v1 mobile

--- caminho/para/modal.js
+++ caminho/para/modal.js
@@ -252,3 +252,24 @@ fetch('estados-cidades.json')
       });
     });
   });
+
+
+ 
+  /* abaixo menu com overlay versao mobile*/ 
+  
+  let btnMenu = document.getElementById('btn-abrir-menu')
+  let menu = document.getElementById('menu-mobile')
+  let overlay = document.getElementById('overlay-menu')
+  
+  btnMenu.addEventListener('click',()=>{
+      menu.classList.add('abrir-menu')
+  })
+  
+  menu.addEventListener('click',()=>{
+      menu.classList.remove('abrir-menu')
+  })
+  
+  overlay.addEventListener('click',()=>{
+      menu.classList.remove('abrir-menu')
+  })
+  
