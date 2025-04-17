3101758d014165efb8ce847659fd943d9907ae9d
tiaxgo <161162502+tiaxgo@users.noreply.github.com>
2025-04-17 14:24:41 -0300

    up

--- caminho/para/modal.js
+++ caminho/para/modal.js
@@ -273,3 +273,21 @@ fetch('estados-cidades.json')
       menu.classList.remove('abrir-menu')
   })
   
+
+  const script_do_google = 'https://script.google.com/macros/s/AKfycbzkiZok2T8K4G6H7v_SdMqs1C-4PbuEvXqe3pxrQs1qjlaolIYbS7VQBs9CJMWPhaEdxg/exec';
+  const dados_do_formulario = document.forms['formulario-contato'];
+  
+  dados_do_formulario.addEventListener('submit', function (e) {
+      e.preventDefault();
+  
+      fetch(script_do_google, { method: 'POST', body: new FormData(dados_do_formulario) })
+          .then(response => {
+              // Se os dados forem gravados corretamente, será enviada uma mensagem de sucesso
+              alert('Dados enviados com sucesso!', response);
+              dados_do_formulario.reset(); 
+          })
+          .catch(error => {
+              // Se houver erro no envio, a menssagem abaixo será exibida
+              console.error('Erro no envio dos dados!', error);
+          });
+  });
